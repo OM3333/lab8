29dfe1b3a3b5f1a6afbf4b6eb0ccc8cad94b010e
Student <lukaslux@outlook.com>
2023-03-07 14:05:11 +0100

    Po zajeciach 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
+    public static final Point test = new Point(2,3);
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println(121233);
+
     }
 }
