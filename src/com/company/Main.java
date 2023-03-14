a61bc3e7d48ebd0b9b895aefa452f5fcbf0c9f90
mpiekarz <monika.piekarz@mail.umcs.pl>
2023-03-14 15:37:52 +0100

    l3 z3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,16 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-        Shape poly = new Polygon(new Vec2[]{new Vec2(120,60), new Vec2(270,280), new Vec2(240,320), new Vec2(110,80)});
+        Shape poly = new Polygon(new Vec2[]{new Vec2(120, 60), new Vec2(270, 280), new Vec2(240, 320), new Vec2(110, 80)});
 
-       Shape ellipse = new Ellipse(new Vec2(2,3),10,20);
-       poly = new SolidFilledShapeDecorator(poly,"red");
-        ellipse = new SolidFilledShapeDecorator(ellipse,"blue");
-//        SvgScene scene=new SvgScene();
+        Shape ellipse = new Ellipse(new Vec2(2, 3), 10, 20);
+        poly = new SolidFilledShapeDecorator(poly, "red");
+        poly = new StrokeShapeDecorator(poly, "green", 24);
+        ellipse = new SolidFilledShapeDecorator(ellipse, "blue");
+        ellipse = new StrokeShapeDecorator(ellipse, "green", 24);
+
+
+        //      SvgScene scene=new SvgScene();
 //        scene.addShape(poly);
 //        scene.saveHtml("scene.html");
     }
