e8320870ea99714c098805607442ff8fcfb84453
Łukasz Kurant <lukaslux@outlook.com>
2023-04-24 19:27:09 +0200

    lab8 prep

--- shop/src/main/java/pl/umcs/oop/lec7/database/DataConnection.java
+++
@@ -1,9 +0,0 @@
-package pl.umcs.oop.lec7.database;
-
-import java.sql.Connection;
-
-public interface DataConnection {
-    public Connection getConnection();
-    public void connect();
-    public void disconnect();
-}
